2e6121d1c05cbd00f703545cd719e1d8bb23bc5c
Ronan Rank-Nemish <ronannemish@gmail.com>
2023-10-07 19:24:06 +0200

    added data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Import all components
-import LinkHub from "./components/LinkHub";
+// import LinkHub from "./components/LinkHub";
+import Link from "./components/Link";
 
 export default function App() {
+  // const [links, setLinks] = useState([]);
+  const data = [
+    {
+      text: "Twitch",
+      link: "https://twitch.com/",
+      icon: "twitch.ico"
+    }
+  ]
   return (
     <>
-      <LinkHub title={'working'} />
+      <ul>{data.map(d => (<a key={d.text} href={d.link} className="btn">{d.text}</a>))}</ul >
     </>
   );
 }
-
-
--- src/components/Link.js
+++ src/components/Link.js
@@ -1,5 +1,5 @@
 export default function Link({ text, link, icon }) {
     return (
-        <a href={link}><img src={icon} />{text}</a>
+        <a href={link}><img src='/src/assets/twitch.ico' />{text}</a>
     );
 }
