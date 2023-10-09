3638126c83a049817f144ee96e74d9158a1167c3
Ronan Rank-Nemish <ronannemish@gmail.com>
2023-10-09 09:07:12 +0200

    Working loops for LinkHub and Link components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import LinkHub from './components/LinkHub';
 import bookmarks from './data/LinkStorage.json';
 import './style.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Import all components
-// import LinkHub from "./components/LinkHub";
-import Link from "./components/Link";
 
 export default function App() {
+  var temp = {};
+  var categories = [];
+
+  for (var bookmark, i = 0; (bookmark = bookmarks[i++]);) {
+    var category = bookmark.Category;
+    if (!(category in temp)) {
+      temp[category] = 1;
+      categories.push(category);
+    }
+  }
+
+  const linkHubs = []
+  for (let i = 0; i < categories.length; i++) {
+    linkHubs.push(<LinkHub data={bookmarks} title={categories[i]} />)
+  }
+
   return (
     <>
-      <div>
-        <ul>{bookmarks.map(d => (<a key={d.Link} href={d.Link} className="links">{d.Text}</a>))}</ul >
+      <div className="container main-box">
+        {categories.map((category, index) => {
+          return <LinkHub key={category} data={bookmarks} title={category} />
+        })}
       </div>
     </>
   );
--- src/components/LinkHub.js
+++ src/components/LinkHub.js
@@ -1,7 +1,16 @@
-export default function LinkHub({ title }) {
+export default function LinkHub({ data, title }) {
+    var filteredData = data.filter(function (e) {
+        return e.Category === title;
+    });
+
     return (
         <>
-            <p>{title}</p>
+            <h4 className="sub-title">{title}</h4>
+            <div className="col-align">
+                <div className="link-hub">
+                    <ul>{filteredData.map(d => (<a key={d.Link} href={d.Link} className="links">{d.Text}</a>))}</ul >
+                </div>
+            </div>
         </>
     );
 }
