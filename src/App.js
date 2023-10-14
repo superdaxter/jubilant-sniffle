2b58c3b2b4df7cc9b94bdc0b1939db139018545e
Ronan Rank-Nemish <ronannemish@gmail.com>
2023-10-14 09:30:44 +0200

    fixed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import LinkHub from './components/LinkHub';
-import bookmarks from './data/LinkStorage.json';
-import './style.css';
+import LinkHub from "./components/LinkHub";
+import bookmarks from "./data/LinkStorage.json";
+import "./style.css";
 
 export default function App() {
   var temp = {};
   var categories = [];
 
-  for (var bookmark, i = 0; (bookmark = bookmarks[i++]);) {
+  for (var bookmark, i = 0; (bookmark = bookmarks[i++]); ) {
     var category = bookmark.Category;
     if (!(category in temp)) {
       temp[category] = 1;
@@ -14,17 +14,20 @@ export default function App() {
     }
   }
 
-  const linkHubs = []
-  for (let i = 0; i < categories.length; i++) {
-    linkHubs.push(<LinkHub data={bookmarks} title={categories[i]} />)
-  }
-
   return (
     <>
       <div className="container main-box">
-        {categories.map((category, index) => {
-          return <LinkHub key={category} data={bookmarks} title={category} />
-        })}
+        <div className="row">
+          {categories.map((category, index) => {
+            return (
+              <>
+                <div className="col-4">
+                  <LinkHub key={index} data={bookmarks} title={category} />
+                </div>
+              </>
+            );
+          })}
+        </div>
       </div>
     </>
   );
--- src/components/LinkHub.js
+++ src/components/LinkHub.js
@@ -1,16 +1,23 @@
 export default function LinkHub({ data, title }) {
-    var filteredData = data.filter(function (e) {
-        return e.Category === title;
-    });
+  var filteredData = data.filter(function (e) {
+    return e.Category === title;
+  });
 
-    return (
-        <>
-            <h4 className="sub-title">{title}</h4>
-            <div className="col-align">
-                <div className="link-hub">
-                    <ul>{filteredData.map(d => (<a key={d.Link} href={d.Link} className="links">{d.Text}</a>))}</ul >
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <h4 className="sub-title">{title}</h4>
+      <div className="col-align">
+        <div className="link-hub">
+          <ul>
+            {filteredData.map((d) => (
+              <a key={d.Link} href={d.Link} className="links">
+                <img src="" alt="" />
+                {d.Text}
+              </a>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
