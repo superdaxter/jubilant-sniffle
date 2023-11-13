14b1d7dc6a69c5f35651ba2907eab70d05685757
Ronan Rank-Nemish <ronannemish@gmail.com>
2023-11-13 21:24:57 +0100

    new logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
+import logo from "../assets/logo.png";
+
 export default function Header() {
   return (
     <>
-      <div className="header">This is a header</div>
+      <div className="header">
+        <img src={logo} alt="logo" width={100}></img>
+      </div>
     </>
   );
 }
